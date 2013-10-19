78ee0381835918dd400e83b41bedc0237f3966a9
Guy Mograbi <guy.mograbi@gmail.com>
2013-10-19 23:54:41 +0300

    removing imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -283,7 +283,6 @@ module.exports = function (grunt) {
             dist: [
                 'coffee',
                 'compass:dist',
-                'imagemin',
                 'htmlmin'
             ]
         },
