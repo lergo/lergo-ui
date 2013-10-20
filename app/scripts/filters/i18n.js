0ef4b1a06426cef40c8f303bdcdc76797eba48b9
Guy Mograbi <guy.mograbi@gmail.com>
2013-10-21 00:27:44 +0300

    fixing filter.

--- app/scripts/filters/i18n.js
+++ app/scripts/filters/i18n.js
@@ -4,7 +4,7 @@ angular.module('lergoApp')
     .filter('i18n', function (LergoTranslate) {
         return function (input, opts ) {
             console.log(opts);
-            if ( opts.skip ){
+            if ( !!opts && !!opts.skip ){
                 return input;
             }else{
                 return LergoTranslate.translate( input);
