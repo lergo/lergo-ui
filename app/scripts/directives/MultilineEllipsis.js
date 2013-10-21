e70ad418bb99730c805dc1dc99b29cd466417156
Guy Mograbi <guy.mograbi@gmail.com>
2013-10-21 23:41:40 +0300

    fixing the multiline implementation.

--- app/scripts/directives/MultilineEllipsis.js
+++ app/scripts/directives/MultilineEllipsis.js
@@ -3,18 +3,16 @@
 angular.module('lergoApp')
     .directive('multilineEllipsis', function () {
         return {
-            template: '<p>{{text}}</p>',
+            template: '<p ng-transclude></p>',
+            transclude:true,
             restrict: 'A',
-            scope:{
-                text:'@'
-            },
             link: function postLink(scope, element/*, attrs*/) {
 
                 function truncate(index, text) {
                     return text.replace(/\W*\s(\S)*$/, '...');
                 }
 
-                scope.$watch('text', function(){
+                scope.$watch(function(){ return element.text() }, function(){
                     var $p = $(element.find('p'));
                     var divh = $(element).height();
                     while ($p.outerHeight() > divh) {
