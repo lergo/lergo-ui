56fb4d90c892f5e5e7e43fa5930b6e2ba532a5b9
guy.mograbi@gmail.com <guymograbi>
2014-04-22 12:44:09 +0300

    fixing indication of "save" LERGO-40

--- app/scripts/controllers/Lesson.js
+++ app/scripts/controllers/Lesson.js
@@ -1,74 +1,93 @@
 'use strict';
 
-angular.module('lergoApp').controller('LessonCtrl', function($scope, $log, LergoClient, $location , $routeParams, ContinuousSave ) {
+angular.module('lergoApp').controller('LessonCtrl', function ($scope, $log, LergoClient, $location, $routeParams, ContinuousSave) {
 
-    var saveLesson = new ContinuousSave( {
-        'saveFn' : function( value ){
+    var saveLesson = new ContinuousSave({
+        'saveFn': function (value) {
             return LergoClient.lessons.update(value);
         }
     });
 
 
-    LergoClient.lessons.getById( $routeParams.lessonId).then( function(result){
+    $scope.isSaving = function(){
+        return !! saveLesson.getStatus().saving;
+    };
+
+
+    LergoClient.lessons.getById($routeParams.lessonId).then(function (result) {
         $scope.lesson = result.data;
         $scope.$watch('lesson', saveLesson.onValueChange, true);
     });
 
 
+    $scope.stepTypes = [
+        {
+            'id': 'video',
+            'label': 'Video'
+        },
+        {
+            'id': 'quiz',
+            'label': 'Quiz'
+        }
+    ];
 
-	$scope.stepTypes = [ {
-		'id' : 'video',
-		'label' : 'Video'
-	}, {
-		'id' : 'quiz',
-		'label' : 'Quiz'
-	} ];
+    $scope.quizTypes = [
+        {
+            'id': 'test',
+            'label': 'Test'
+        },
+        {
+            'id': 'exercise',
+            'label': 'Exercise'
+        }
+    ];
 
-	$scope.quizTypes = [ {
-		'id' : 'test',
-		'label' : 'Test'
-	}, {
-		'id' : 'exercise',
-		'label' : 'Exercise'
-	} ];
+    $scope.addStep = function (lesson) {
+        if (!lesson.steps) {
+            lesson.steps = [];
+        }
 
-	$scope.addStep = function(lesson) {
-		if (!lesson.steps) {
-			lesson.steps = [];
-		}
+        lesson.steps.push({});
+    };
+    $scope.moveStepUp = function (index) {
+        var temp = $scope.lesson.steps[index - 1];
+        if (temp) {
+            $scope.lesson.steps[index - 1] = $scope.lesson.steps[index];
+            $scope.lesson.steps[index] = temp;
+        }
+    };
+    $scope.moveStepDown = function (index) {
+        var temp = $scope.lesson.steps[index + 1];
+        if (temp) {
+            $scope.lesson.steps[index + 1] = $scope.lesson.steps[index];
+            $scope.lesson.steps[index] = temp;
+        }
 
-		lesson.steps.push({});
-	};
-	$scope.moveStepUp = function(index) {
-		var temp = $scope.lesson.steps[index - 1];
-		if (temp) {
-			$scope.lesson.steps[index - 1] = $scope.lesson.steps[index];
-			$scope.lesson.steps[index] = temp;
-		}
-	};
-	$scope.moveStepDown = function(index) {
-		var temp = $scope.lesson.steps[index + 1];
-		if (temp) {
-			$scope.lesson.steps[index + 1] = $scope.lesson.steps[index];
-			$scope.lesson.steps[index] = temp;
-		}
-
-	};
+    };
 
 
-    $scope.done = function(){
+    $scope.done = function () {
         $location.path('/user/lessons');
     };
 
-	$scope.getStepViewByType = function(step) {
-		var type = 'none';
-		if (!!step && !!step.type) {
-			type = step.type;
-		}
-		return 'views/lesson/steps/_' + type + '.html';
-	};
+    $scope.getStepViewByType = function (step) {
+        var type = 'none';
+        if (!!step && !!step.type) {
+            type = step.type;
+        }
+        return 'views/lesson/steps/_' + type + '.html';
+    };
+
+    LergoClient.questions.getUserQuestions().then(function (result) {
+        $scope.quizItems = result.data;
+    });
 
 
+    $scope.addItemToQuiz = function( itemId , step ){
+        step.quizItems = step.quizItems || [];
+
+        step.quizItems.push( itemId );
+    }
 
 
 });
