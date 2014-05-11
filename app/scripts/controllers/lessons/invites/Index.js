41ad53309272e2c17cc7cf53dd284a5631ce65b5
guy.mograbi@gmail.com <guymograbi>
2014-05-11 09:14:24 +0300

    adding infrastructure to invite users for lessons.

---
+++ app/scripts/controllers/lessons/invites/Index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('lergoApp')
+  .controller('LessonsInvitesIndexCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
