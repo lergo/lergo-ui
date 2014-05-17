b62581219a7b7a3de6f07cd5c947c17012da0f51
shukla2009 <shukla2009@gmail.com>
2014-05-18 03:58:00 +0530

    builfix

--- app/scripts/services/VideoService.js
+++ app/scripts/services/VideoService.js
@@ -3,7 +3,7 @@
 angular.module('lergoApp').service('VideoService', function VideoService() {
 	this.getMedia = function(url) {
 		var media = {};
-		if (url.match('http|https://(www.)?youtube|youtu\.be')) {
+		if (url.match('http|https://(www.)?youtube|youtu.be')) {
 			media.type = 'youtube';
 			if (url.match('embed')) {
 				media.id = url.split(/embed\//)[1].split('"')[0];
@@ -11,12 +11,10 @@ angular.module('lergoApp').service('VideoService', function VideoService() {
 				media.id = url.split(/v\/|v=|youtu\.be\//)[1].split(/[?&]/)[0];
 			}
 			return media;
-		} else if (url.match('http://(player.)?vimeo\.com')) {
+		} else if (url.match('http://(player.)?vimeo.com')) {
 			media.id = url.split(/video\/|http:\/\/vimeo\.com\//)[1].split(/[?&]/)[0];
 			media.type = 'vimeo';
 			return media;
-		} else {
-			alert('No valid media id detected');
 		}
 		return null;
 	};
