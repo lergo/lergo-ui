c2a452a24cdd47f0bd805cbd98fef2f935a56560
guy.mograbi@gmail.com <guymograbi>
2014-06-14 21:44:29 +0300

    fixing build.

--- app/scripts/directives/adminSection.js
+++ app/scripts/directives/adminSection.js
@@ -5,9 +5,6 @@ angular.module('lergoApp')
         return {
             template: '<div ng-transclude ng-show="user.isAdmin"></div>',
             restrict: 'A',
-            transclude: true,
-            link: function postLink(scope, element, attrs) {
-
-            }
+            transclude: true
         };
     });
