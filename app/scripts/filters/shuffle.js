8995fa0ce2461a231365f80af41d394e86bf2d9b
guy.mograbi@gmail.com <guymograbi>
2014-06-23 21:22:22 +0300

    adding shuffle for multi choice.

---
+++ app/scripts/filters/shuffle.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('lergoApp')
+    .filter('shuffle', function ( $log ) {
+        return function (array) {
+            if ( !!array.isShuffled ){ // hack to get this filter to run once.
+                return array;
+            }
+
+            $log.info('shuffling');
+            var m = array.length, t, i;
+
+            // While there remain elements to shuffle
+            while (m) {
+                // Pick a remaining element…
+                i = Math.floor(Math.random() * m--);
+
+                // And swap it with the current element.
+                t = array[m];
+                array[m] = array[i];
+                array[i] = t;
+            }
+
+            array.isShuffled =true;
+            return array;
+        };
+    });
