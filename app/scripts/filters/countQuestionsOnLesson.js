49327c4ce3d48b359edd734aa5141db50d014a81
guy.mograbi@gmail.com <guymograbi>
2014-06-27 09:45:27 +0300

    filter for counting questions on lesson.

---
+++ app/scripts/filters/countQuestionsOnLesson.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('lergoApp')
+    .filter('countQuestionsOnLesson', function () {
+
+        return function (item) {
+                var qCount = 0;
+                if (!item || !item.steps ) {
+                    return qCount;
+                }
+                for (var i = 0; i < item.steps.length; i++) {
+                    if (!!item.steps[i].quizItems) {
+                        qCount = qCount + item.steps[i].quizItems.length;
+                    }
+                }
+                return qCount;
+
+        };
+    });
