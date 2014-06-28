37ac1fd8cab0f02e61dfe212343f404422ac23f2
guy.mograbi@gmail.com <guymograbi>
2014-06-28 16:06:46 +0300

    LERGO-210 - fixing build.

--- app/scripts/directives/baseLayout.js
+++ app/scripts/directives/baseLayout.js
@@ -1,55 +1,58 @@
 'use strict';
 
-angular.module('lergoApp').directive('baseLayout', function($rootScope, $log, $location, LergoClient, LergoTranslate) {
-	return {
-		templateUrl : '/views/baseLayout.html',
-		transclude : true,
-		restrict : 'C',
-		replace : true,
-		link : function postLink(scope/* , element /*, attrs */) {
-			LergoClient.isLoggedIn().then(function(result) {
-				if (!!result) {
-					$rootScope.user = result.data;
-				}
-			});
+angular.module('lergoApp').directive('baseLayout', function ($rootScope, $log, $location, LergoClient, LergoTranslate) {
+    return {
+        templateUrl: '/views/baseLayout.html',
+        transclude: true,
+        restrict: 'C',
+        replace: true,
+        link: function postLink(scope/* , element /*, attrs */) {
+            LergoClient.isLoggedIn().then(function (result) {
+                if (!!result) {
+                    $rootScope.user = result.data;
+                }
+            });
 
 
             // a random number to prevent cache in ng-include
-            $rootScope.getCacheRandomNumber = function(){
+            $rootScope.getCacheRandomNumber = function () {
                 return Math.floor(new Date().getTime() / 10000);
             };
 
-			$rootScope.getLabelForLanguage = function(id) {
+            $rootScope.getLabelForLanguage = function (id) {
 
-				return LergoTranslate.translate('translationLanguage.' + id);
-			};
-			$rootScope.lergoLanguages = [
+                return LergoTranslate.translate('translationLanguage.' + id);
+            };
+            $rootScope.lergoLanguages = [
                 {
-				'id' : 'en',
-				'label' : 'English'
-			}, {
-				'id' : 'he',
-				'label' : 'Hebrew'
-			    },{
+                    'id': 'en',
+                    'label': 'English'
+                },
+                {
+                    'id': 'he',
+                    'label': 'Hebrew'
+                },
+                {
                     'id': 'ru',
-                    'label' : 'Russian'
-                },{
-                    'id' : 'ar',
-                    'label' : 'Arabic'
+                    'label': 'Russian'
+                },
+                {
+                    'id': 'ar',
+                    'label': 'Arabic'
                 }
             ];
 
-			$rootScope.$watch('lergoLanguage', function(newValue/* , oldValue */) {
-				$log.info('new language', newValue);
-				LergoTranslate.setLanguage(newValue);
-			});
+            $rootScope.$watch('lergoLanguage', function (newValue/* , oldValue */) {
+                $log.info('new language', newValue);
+                LergoTranslate.setLanguage(newValue);
+            });
 
-			scope.logout = function() {
-				LergoClient.logout().then(function() {
-					$rootScope.user = null;
-					$location.path('/');
-				});
-			};
-		}
-	};
+            scope.logout = function () {
+                LergoClient.logout().then(function () {
+                    $rootScope.user = null;
+                    $location.path('/');
+                });
+            };
+        }
+    };
 });
