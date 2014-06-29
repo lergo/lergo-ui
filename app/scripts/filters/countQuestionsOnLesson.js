f3fa054581a51ed77faa9ece52b66138ca020d6d
guy.mograbi@gmail.com <guymograbi>
2014-06-29 07:41:58 +0300

    LERGO-217 - fixed.

--- app/scripts/filters/countQuestionsOnLesson.js
+++ app/scripts/filters/countQuestionsOnLesson.js
@@ -5,13 +5,17 @@ angular.module('lergoApp')
 
         return function (item) {
             var qCount = 0;
-            if (!item || !item.steps) {
-                return qCount;
-            }
-            for (var i = 0; i < item.steps.length; i++) {
-                if (!!item.steps[i].quizItems) {
-                    qCount = qCount + item.steps[i].quizItems.length;
+            try {
+                if (!item || !item.steps) {
+                    return qCount;
                 }
+                for (var i = 0; i < item.steps.length; i++) {
+                    if (!!item.steps[i].quizItems) {
+                        qCount = qCount + item.steps[i].quizItems.length;
+                    }
+                }
+            }catch(e){
+                return qCount;
             }
             return qCount;
 
