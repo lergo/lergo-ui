bddba5c6ca4a6fa0364a46c71f0dbc28734fd689
shukla2009 <shukla2009@gmail.com>
2014-07-10 22:26:49 +0530

    build-fix

--- app/scripts/directives/setfocus.js
+++ app/scripts/directives/setfocus.js
@@ -3,15 +3,18 @@
 angular.module('lergoApp').directive('setFocus', function($parse) {
 	return {
 		restrict : 'A',
-		link : {
-			post : function(scope, element, attrs) {
-				var model = $parse(attrs.setFocus);
-				scope.$watch(model, function(value) {
-					if (value === true) {
-						element[0].focus();
-					}
-				});
-			}
+		link : function(scope, element, attrs) {
+			var model = $parse(attrs.setFocus);
+			scope.$watch(model, function(value) {
+				if (value === true) {
+					element[0].focus();
+				}
+			});
+			element.bind('blur', function() {
+				if (!!model) {
+					scope.$apply(model.assign(scope, false));
+				}
+			});
 		}
 	};
 });
