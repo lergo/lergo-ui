7bfc16d9b599d0e439aa8e1bd767a6922fc78d99
shukla2009 <shukla2009@gmail.com>
2014-07-10 21:43:50 +0530

    lergo-249 : persist scroll position fill in the blanks default focus on first box lergo-246 review comment (show saved/saving next to done button)

---
+++ app/scripts/directives/setfocus.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('lergoApp')
+  .directive('setFocus', function ($parse) {
+	  return {
+	        restrict: 'A',
+	        link: function (scope, element, attrs) {
+	            var model = $parse(attrs.setFocus);
+	            scope.$watch(model, function (value) {
+	                if (value === true) {
+	                    element[0].focus();
+	                }
+	            });
+	            element.bind('blur', function () {
+	                scope.$apply(model.assign(scope, false));
+	            });
+	        }
+	    };
+  });
