9c30e54c9ccca97a29081ab26f192a61ca24bdad
shukla2009 <shukla2009@gmail.com>
2014-07-14 22:44:14 +0530

    lergo-267

--- app/scripts/directives/quizItemMedia.js
+++ app/scripts/directives/quizItemMedia.js
@@ -14,12 +14,31 @@ angular.module('lergoApp').directive('quizItemMedia', function($sce, $compile, $
 			$scope.getMediaTemplate = function() {
 				var quizItem = $scope.quizItem;
 				var type = 'none';
-				if (!!quizItem.media && !!quizItem.media.type && (!!quizItem.media.audioUrl || !! quizItem.media.imageUrl)) {
+				if (!!quizItem.media && !!quizItem.media.type && isValid(quizItem.media)) {
 					type = quizItem.media.type;
 				}
 				return 'views/questions/view/media/_' + type + '.html';
 			};
+			function isValid(media) {
+				if (!media) {
+					return false;
+				}
+				if (media.type === 'image')
+					return isValidImage(media.imageUrl);
+				else if (media.type === 'audio') {
+					return isValidAudio(media.audioUrl);
+				}
+			}
 
+			function isValidImage(url) {
+				return !!url;
+
+			}
+
+			function isValidAudio(url) {
+				return !!url;
+
+			}
 		},
 		template : '<div ng-include=getMediaTemplate()></div>'
 	};
