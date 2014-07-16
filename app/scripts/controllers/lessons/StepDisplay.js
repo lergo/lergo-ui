7ed1965f599f49ec69f0c0382a0ec1b805d94940
shukla2009 <shukla2009@gmail.com>
2014-07-16 21:23:54 +0530

    lergo-247 review comments

--- app/scripts/controllers/lessons/StepDisplay.js
+++ app/scripts/controllers/lessons/StepDisplay.js
@@ -165,5 +165,22 @@ angular.module('lergoApp').controller('LessonsStepDisplayCtrl', function($scope,
 		}
 		return LergoClient.questions.getTypeById(quizItem.type).canSubmit(quizItem);
 	};
+	
+	$scope.getFillIntheBlankSize = function(quizItem, index) {
+		if (!quizItem.blanks || !quizItem.blanks.type || quizItem.blanks.type == "auto") {
+			if (!!quizItem.answer[index]) {
+				var answer = quizItem.answer[index].split(';');
+				var maxLength = 0;
+				for ( var i = 0; i < answer.length; i++) {
+					if (answer[i].length > maxLength) {
+						maxLength = answer[i].length;
+					}
+				}
+				return maxLength * 10 + 20;
+			}
+		} else if (quizItem.blanks.type == "custom") {
+			return quizItem.blanks.size * 10 + 20;
+		}
+	};
 
 });
