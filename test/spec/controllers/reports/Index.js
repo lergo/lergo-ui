502301ccd573e7612d2d2d5bb5652352e21827a3
shukla2009 <shukla2009@gmail.com>
2014-08-13 18:11:03 +0530

    buildfix

--- test/spec/controllers/reports/Index.js
+++ test/spec/controllers/reports/Index.js
@@ -1,22 +1,21 @@
 'use strict';
 
-describe('Controller: ReportsIndexCtrl', function () {
+describe('Controller: ReportsIndexCtrl', function() {
 
-  // load the controller's module
-  beforeEach(module('lergoApp'));
+	// load the controller's module
+	beforeEach(module('lergoApp'));
 
-  var ReportsIndexCtrl,
-    scope;
+	var ReportsIndexCtrl, scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    ReportsReportindexctrlCtrl = $controller('ReportsIndexCtrl', {
-      $scope: scope
-    });
-  }));
+	// Initialize the controller and a mock scope
+	beforeEach(inject(function($controller, $rootScope) {
+		scope = $rootScope.$new();
+		ReportsIndexCtrl = $controller('ReportsIndexCtrl', {
+			$scope : scope
+		});
+	}));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+	it('should attach a list of awesomeThings to the scope', function() {
+		expect(scope.awesomeThings.length).toBe(3);
+	});
 });
