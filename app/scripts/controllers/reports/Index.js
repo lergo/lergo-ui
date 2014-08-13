ee3a4697b39ecb6965d7bd39672222e03c9d3430
shukla2009 <shukla2009@gmail.com>
2014-08-13 17:40:39 +0530

    build-fix

--- app/scripts/controllers/reports/Index.js
+++ app/scripts/controllers/reports/Index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('lergoApp').controller('ReportsIndexCtrl', function($scope,LergoClient,TagsService,FilterService,$log) {
+angular.module('lergoApp').controller('ReportsIndexCtrl', function($scope, LergoClient, TagsService, FilterService, $log) {
 
 	$scope.languageFilter = function(report) {
 		return FilterService.filterByLanguage(report.data.lesson.language);
@@ -9,14 +9,13 @@ angular.module('lergoApp').controller('ReportsIndexCtrl', function($scope,LergoC
 		return FilterService.filterBySubject(report.data.lesson.subject);
 	};
 
-    $scope.tagsFilter = function(report){
-        return FilterService.filterByTags(report.data.lesson.tags);
-    };
-    $scope.studentFilter = function(report){
-        return FilterService.filterByInvitee(report.data.invitee.name);
-    };
-    
-	
+	$scope.tagsFilter = function(report) {
+		return FilterService.filterByTags(report.data.lesson.tags);
+	};
+	$scope.studentFilter = function(report) {
+		return FilterService.filterByInvitee(report.data.invitee.name);
+	};
+
 	$scope.selectAll = function(event) {
 		var checkbox = event.target;
 		if (checkbox.checked) {
@@ -43,8 +42,10 @@ angular.module('lergoApp').controller('ReportsIndexCtrl', function($scope,LergoC
 				continue;
 			} else if (!FilterService.filterByTags(items[i].data.lesson.tags)) {
 				continue;
-//			} else if (!FilterService.filterByCorrectPercentage(items[i].correctPercentage)) {
-//				continue;
+				// } else if
+				// (!FilterService.filterByCorrectPercentage(items[i].correctPercentage))
+				// {
+				// continue;
 			} else {
 				filteredItems.push(items[i]);
 			}
@@ -52,20 +53,19 @@ angular.module('lergoApp').controller('ReportsIndexCtrl', function($scope,LergoC
 		return filteredItems;
 
 	}
-	
-	
+
 	$scope.getAll = function() {
 		LergoClient.userData.getReports().then(function(result) {
 			$scope.reports = result.data;
 			$scope.errorMessage = null;
-            $scope.availableTags = TagsService.getTagsFromItems( $scope.reports );
-            $scope.invitees = [];
-            angular.forEach($scope.reports, function(item) {
-            	if ($scope.invitees.indexOf(item.data.invitee.name) === -1) {
-                    $scope.invitees.push(item.data.invitee.name);
-                }
+			$scope.availableTags = TagsService.getTagsFromItems($scope.reports);
+			$scope.invitees = [];
+			angular.forEach($scope.reports, function(item) {
+				if ($scope.invitees.indexOf(item.data.invitee.name) === -1) {
+					$scope.invitees.push(item.data.invitee.name);
+				}
 			});
-            
+
 		}, function(result) {
 			$scope.errorMessage = 'Error in fetching reports : ' + result.data.message;
 			$log.error($scope.errorMessage);
