269afb94afcc896060d3864dd2dbb2b497c4d835
guy.mograbi@gmail.com <guymograbi>
2014-09-27 15:56:25 +0300

    LERGO-445 - fixing number of questions in homepage - improved code for "intro" page as well..

--- app/scripts/filters/countQuestionsOnLesson.js
+++ app/scripts/filters/countQuestionsOnLesson.js
@@ -4,16 +4,16 @@ angular.module('lergoApp')
     .filter('countQuestionsOnLesson', function () {
 
         return function (item) {
-
-            if ( item.hasOwnProperty('questionsCount') ){
-                return item.questionsCount;
-            }
-
             var qCount = 0;
             try {
                 if (!item || !item.steps) {
                     return qCount;
                 }
+
+                if ( item.hasOwnProperty('questionsCount') ){
+                    return item.questionsCount;
+                }
+
                 for (var i = 0; i < item.steps.length; i++) {
                     if (!!item.steps[i].quizItems) {
                         qCount = qCount + item.steps[i].quizItems.length;
