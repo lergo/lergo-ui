621a3810385ef194891a812413bceb641699e159
shukla2009 <shukla2009@gmail.com>
2014-10-08 02:54:45 +0530

    lergo-493

--- app/scripts/controllers/lessons/invitations/Report.js
+++ app/scripts/controllers/lessons/invitations/Report.js
@@ -1,19 +1,21 @@
 'use strict';
 
-angular.module('lergoApp').controller('LessonsInvitationsReportCtrl', function($scope, $log, LergoClient, $routeParams) {
+angular.module('lergoApp').controller('LessonsInvitationsReportCtrl', function($scope, $log, LergoClient, $routeParams, $rootScope) {
 	$log.info('loading');
 	LergoClient.reports.getById($routeParams.reportId).then(function(result) {
 		$scope.report = result.data;
+		$rootScope.page = {
+			'title' : $scope.report.data.lesson.name,
+			'description' : $scope.report.data.lesson.description
+		};
 	});
-	$scope.stats=[];
+	$scope.stats = [];
 	$scope.$on('stats', function(event, data) {
 		$scope.stats[data.index] = data;
 	});
 
-
-
 	$scope.absoluteShareLink = function(id) {
-        return window.location.origin + '/#!/public/lessons/' + id + '/intro';
+		return window.location.origin + '/#!/public/lessons/' + id + '/intro';
 	};
 
 });
