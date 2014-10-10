19fd6c6ec4d846983202ecbe1fcc98fe986501ba
guy.mograbi@gmail.com <guymograbi>
2014-10-10 17:05:50 +0300

    chore(filter load test): write test
    TESTS LERGO-490

--- test/spec/directives/lergoFilter.js
+++ test/spec/directives/lergoFilter.js
@@ -1,15 +1,32 @@
 'use strict';
 
 describe('Directive: lergoFilter', function () {
-    beforeEach(module('lergoApp', 'directives-templates', 'lergoBackendMock'));
+    beforeEach(module('lergoApp', 'directives-templates', 'lergoBackendMock', 'LocalStorageModule'));
 
     var element;
 
-    it('should make hidden element visible', inject(function ($rootScope, $compile) {
+    it('should have a form in display', inject(function ($rootScope, $compile) {
         $rootScope.model = {};
         element = angular.element('<div lergo-filter model="model"></div>');
         element = $compile(element)($rootScope);
         $rootScope.$digest();
         expect($(element).find('form').length === 1 || $(element).is('form')).toBe(true);
+    }));
+
+
+    // tests that fix to LERGO-490 applies.
+    // the fix contained
+    it('should trigger watch functions when loading from local storage', inject(function($rootScope, localStorageService, $timeout, $compile){
+        $rootScope.model = {};
+        $rootScope.opts = { 'showAge' : true };
+        localStorageService.set('lergoFilter.ageFilter', { 'min' : 10 });
+        element = angular.element('<div lergo-filter opts="opts" model="model"></div>');
+        element = $compile(element)($rootScope);
+
+        $rootScope.$digest();
+        $timeout.flush();
+        /*jshint camelcase: false */
+        expect(element.children().scope().model.age.dollar_gte).toBe(10);
+
     }));
 });
