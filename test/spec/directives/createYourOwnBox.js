765ef211d6207f2f5b9db93a8b77ae3454d24ba0
guy.mograbi@gmail.com <guymograbi>
2014-10-10 12:24:01 +0300

    chore(travis): reference chrome

--- test/spec/directives/createYourOwnBox.js
+++ test/spec/directives/createYourOwnBox.js
@@ -15,5 +15,6 @@ describe('Directive: createYourOwnBox', function () {
         element = $compile(element)($rootScope);
         $rootScope.$digest();
         expect(typeof(element.children().scope().create)).toBe('function');
+        expect(1).toBe(2);
     }));
 });
