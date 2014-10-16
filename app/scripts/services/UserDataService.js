00d195fc2f32c8f1b302cd25bc6fbf3f58853978
shukla2009 <shukla2009@gmail.com>
2014-10-16 05:51:40 +0530

    lergo-512

--- app/scripts/services/UserDataService.js
+++ app/scripts/services/UserDataService.js
@@ -1,31 +1,48 @@
 'use strict';
 
-angular.module('lergoApp')
-    .service('UserDataService', function UserDataService($http) {
+angular.module('lergoApp').service('UserDataService', function UserDataService($http) {
 
-        this.getLessons = function ( queryObj ) {
-            return $http({ 'method' : 'GET' , 'url' : '/backend/user/me/lessons' , 'params' : { 'query' : queryObj }});
-        };
-        // AngularJS will instantiate a singleton by calling "new" on this function
+	this.getLessons = function(queryObj) {
+		return $http({
+			'method' : 'GET',
+			'url' : '/backend/user/me/lessons',
+			'params' : {
+				'query' : queryObj
+			}
+		});
+	};
+	// AngularJS will instantiate a singleton by calling "new" on this
+	// function
 
-        this.getReports = function ( queryObj ) {
-            return $http({
-                'method' : 'GET',
-                'url' : '/backend/user/me/reports',
-                'params' : {
-                    'query' : queryObj
-                }
-            });
-        };
+	this.getReports = function(queryObj) {
+		return $http({
+			'method' : 'GET',
+			'url' : '/backend/user/me/reports',
+			'params' : {
+				'query' : queryObj
+			}
+		});
+	};
 
-        this.getStudentsReports = function (queryObj) {
-            return $http({
-                'method': 'GET',
-                'url': '/backend/user/me/studentsReports',
-                'params': {
-                    'query': queryObj
-                }
+	this.getStudentsReports = function(queryObj) {
+		return $http({
+			'method' : 'GET',
+			'url' : '/backend/user/me/studentsReports',
+			'params' : {
+				'query' : queryObj
+			}
 
-            });
-        };
-    });
+		});
+	};
+
+	this.getInvites = function(queryObj) {
+		return $http({
+			'method' : 'GET',
+			'url' : '/backend/user/me/invites',
+			'params' : {
+				'query' : queryObj
+			}
+
+		});
+	};
+});
