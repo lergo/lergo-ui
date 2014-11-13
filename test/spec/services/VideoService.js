c7b3c3c53b674e6aaf5a1ec45a2a46045a767f52
guy.mograbi@gmail.com <guymograbi>
2014-11-13 09:25:59 +0200

    fix upgrade script add test to VideoService

--- test/spec/services/VideoService.js
+++ test/spec/services/VideoService.js
@@ -15,4 +15,28 @@ describe('Service: VideoService', function () {
         expect(!!mVideoService).toBe(true);
     });
 
+    it('should get media', function () {
+        var media = mVideoService.getMedia('https://www.youtube.com/watch?v=heUd26tuinw');
+        expect(media.type).toBe('youtube');
+        expect(media.id).toBe('heUd26tuinw');
+
+
+    });
+
+    it('should support /embed/ urls from youtube', function () {
+        var media = mVideoService.getMedia('http://www.youtube.com/embed/heUd26tuinw');
+        expect(media.type).toBe('youtube');
+        expect(media.id).toBe('heUd26tuinw');
+    });
+
+    it('should support vimeo urls', function () {
+        var media = mVideoService.getMedia('http://vimeo.com/740052');
+        expect(media.type).toBe('vimeo');
+        expect(media.id).toBe('740052');
+    });
+
+    it('should return null for unknown urls', function(){
+        expect(mVideoService.getMedia('http://gjkrgjrkgjkrjgkr')).toBe(null);
+    });
+
 });
