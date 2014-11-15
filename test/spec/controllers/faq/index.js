18aafa26eb50100ce953dc271885ff5f24f43847
shukla2009 <shukla2009@gmail.com>
2014-11-15 06:16:49 +0530

    build-fix

--- test/spec/controllers/faq/index.js
+++ test/spec/controllers/faq/index.js
@@ -1,22 +1,21 @@
 'use strict';
 
-describe('Controller: FaqIndexCtrl', function () {
+describe('Controller: FaqIndexCtrl', function() {
 
-  // load the controller's module
-  beforeEach(module('lergoApp'));
+	// load the controller's module
+	beforeEach(module('lergoApp'));
 
-  var FaqIndexCtrl,
-    scope;
+	var FaqIndexCtrl, scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    FaqIndexCtrl = $controller('FaqIndexCtrl', {
-      $scope: scope
-    });
-  }));
+	// Initialize the controller and a mock scope
+	beforeEach(inject(function($controller, $rootScope) {
+		scope = $rootScope.$new();
+		FaqIndexCtrl = $controller('FaqIndexCtrl', {
+			$scope : scope
+		});
+	}));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+	it('should attach a list of awesomeThings to the scope', function() {
+		expect(scope.create()).toBe('function');
+	});
 });
