cd69ea2ec5ecc680d476d4f5c11f635a0f3061ab
guy.mograbi@gmail.com <guymograbi>
2014-12-20 21:07:38 +0200

    task(test) - fix baseLayoutFooter test.

--- test/spec/controllers/BaseLayoutFooter.js
+++ test/spec/controllers/BaseLayoutFooter.js
@@ -5,17 +5,28 @@ describe('Controller: BaseLayoutFooterCtrl', function() {
 	// load the controller's module
 	beforeEach(module('lergoApp'));
 
-	var Ctrl, scope;
+	var Ctrl, scope, windowMock;
 
 	// Initialize the controller and a mock scope
 	beforeEach(inject(function($controller, $rootScope) {
 		scope = $rootScope.$new();
+
+        function WindowMock(){
+            this.scrollInvoked = false;
+            this.scrollTo =  function(){
+                this.scrollInvoked = true;
+            };
+
+        }
+        windowMock = new WindowMock();
+
 		Ctrl = $controller('BaseLayoutFooterCtrl', {
-			$scope : scope
+			$scope : scope,
+            $window : windowMock
 		});
 	}));
 
 	it('should test for function', function() {
-		expect(scope.scrollUp).toBe('function');
+        expect(windowMock.scrollInvoked).toBe(true);
 	});
 });
