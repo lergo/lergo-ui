e55f6f9725412f1db181605804c1a1a3803b3bae
Mukta Garodia <mukta.garodia@gmail.com>
2015-12-26 11:24:50 +0200

    add resetFilter notification on lessons page.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -220,7 +220,7 @@ angular.module('lergoApp').directive('tooltip', function () {
 
 angular.module('lergoApp').run(function (Facebook) {
     //if ( typeof(conf) !== 'undefined' ) {
-        Facebook.init(conf.facebookAppId);
+    Facebook.init(conf.facebookAppId);
     //}else{
     //    window.location='/error.html';
     //    throw new Error('service is down');
