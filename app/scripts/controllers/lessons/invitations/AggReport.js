d9e7fca1e6c91d1e92a8fbf35ebc665ee443df2a
Rahul Shukla <shukla2009@gmail.com>
2016-08-18 01:22:32 +0530

    ui changes to display aggregated reports

---
+++ app/scripts/controllers/lessons/invitations/AggReport.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('lergoApp').controller('LessonsInvitationsAggReportCtrl', function ($scope, $log, LergoClient, $routeParams, LergoTranslate, $location, $filter, $rootScope) {
+    $log.info('loading');
+    LergoClient.reports.getClassReportById($routeParams.reportId).then(function (result) {
+        $scope.report = result.data;
+
+        var openQueestions = _.filter($scope.report.data.quizItems, function (item) {
+            return item.type === 'openQuestion';
+        });
+        $scope.openQuestionCount =openQueestions.length;
+            $rootScope.page = {
+            'title': $scope.report.data.lesson.name,
+            'description': $scope.report.data.lesson.description
+        };
+        LergoTranslate.setLanguageByName($scope.report.data.lesson.language);
+    });
+
+    $scope.getStepAvgDuration = function (index) {
+        if (!!$scope.report) {
+            var stepData = $scope.report.stepDurations[index];
+            if (!!stepData) {
+                return stepData.duration / stepData.count;
+            }
+        }
+    };
+    $scope.absoluteShareLink = function (id) {
+        return window.location.origin + '/#!/public/lessons/' + id + '/intro';
+    };
+
+    $scope.getStepViewByType = function (step) {
+        return '/views/lessons/invitations/report/steps/_' + step.type + '.html';
+    };
+
+
+});
