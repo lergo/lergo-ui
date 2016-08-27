8c38a15bf7bb508e173fa34cc95745604d5b8c8c
Rahul Shukla <shukla2009@gmail.com>
2016-08-27 15:38:55 +0530

    Fill in the blanks type question are not rendered properly

--- app/scripts/controllers/lessons/invitations/AggReport.js
+++ app/scripts/controllers/lessons/invitations/AggReport.js
@@ -2,47 +2,56 @@
 
 angular.module('lergoApp').controller('LessonsInvitationsAggReportCtrl',
     function ($scope, $log, LergoClient, $routeParams, LergoTranslate, $location, $filter, $rootScope) {
-    $log.info('loading');
-    LergoClient.reports.getClassReportById($routeParams.reportId).then(function (result) {
-        $scope.report = result.data;
+        $log.info('loading');
+        LergoClient.reports.getClassReportById($routeParams.reportId).then(function (result) {
+            $scope.report = result.data;
 
-        var openQueestions = _.filter($scope.report.data.quizItems, function (item) {
-            return item.type === 'openQuestion';
+            var openQueestions = _.filter($scope.report.data.quizItems, function (item) {
+                return item.type === 'openQuestion';
+            });
+            $scope.openQuestionCount = openQueestions.length;
+            $rootScope.page = {
+                'title': $scope.report.data.lesson.name,
+                'description': $scope.report.data.lesson.description
+            };
+            LergoTranslate.setLanguageByName($scope.report.data.lesson.language);
         });
-        $scope.openQuestionCount = openQueestions.length;
-        $rootScope.page = {
-            'title': $scope.report.data.lesson.name,
-            'description': $scope.report.data.lesson.description
-        };
-        LergoTranslate.setLanguageByName($scope.report.data.lesson.language);
-    });
 
-    $scope.getStepAvgDuration = function (index) {
-        if (!!$scope.report) {
-            var stepData = $scope.report.stepDurations[index];
-            if (!!stepData) {
-                return stepData.duration / stepData.count;
+        $scope.getStepAvgDuration = function (index) {
+            if (!!$scope.report) {
+                var stepData = $scope.report.stepDurations[index];
+                if (!!stepData) {
+                    return stepData.duration / stepData.count;
+                }
             }
-        }
-    };
-    $scope.absoluteShareLink = function (id) {
-        return window.location.origin + '/#!/public/lessons/' + id + '/intro';
-    };
+        };
 
-    $scope.getStepViewByType = function (step) {
-        return '/views/lessons/invitations/report/steps/_' + step.type + '.html';
-    };
+        $scope.getStepCorrectPercentage = function (index) {
+            if (!!$scope.report) {
+                var stepData = $scope.report.stepDurations[index];
+                if (!!stepData) {
+                    return stepData.correctAnswers / stepData.totalAnswers;
+                }
+            }
+        };
+        $scope.absoluteShareLink = function (id) {
+            return window.location.origin + '/#!/public/lessons/' + id + '/intro';
+        };
 
-    $scope.redirectToIndividualReports = function() {
-        $location.path('/user/create/reports').search({
-            reportType: 'students',
-            'lergoFilter.reportClass': $scope.report.data.invitee.class,
-            'lergoFilter.reportLesson': JSON.stringify({
-                _id: $scope.report.data.lesson._id,
-                name: $scope.report.data.lesson.name
-            })
-        });
-    };
+        $scope.getStepViewByType = function (step) {
+            return '/views/lessons/invitations/report/steps/_' + step.type + '.html';
+        };
+
+        $scope.redirectToIndividualReports = function () {
+            $location.path('/user/create/reports').search({
+                reportType: 'students',
+                'lergoFilter.reportClass': $scope.report.data.invitee.class,
+                'lergoFilter.reportLesson': JSON.stringify({
+                    _id: $scope.report.data.lesson._id,
+                    name: $scope.report.data.lesson.name
+                })
+            });
+        };
 
 
-});
+    });
