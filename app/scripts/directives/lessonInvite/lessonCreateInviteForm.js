87e0f2ae291c113862f0e378d3cc2c491834e68f
jlevym <jlevym@gmail.com>
2016-09-16 09:50:01 +0300

    fix: LERGO-650 layout of invite at end of lesson
    the layout of invite before the lesson is played is good
    any change made to the layout after lesson interfered with
    before lesson, so a new invite page was created for after lesson

--- app/scripts/directives/lessonInvite/lessonCreateInviteForm.js
+++ app/scripts/directives/lessonInvite/lessonCreateInviteForm.js
@@ -146,3 +146,48 @@ angular.module('lergoApp').directive('lessonCreateInviteForm', function() {
         }
     };
 });
+
+/*jeff: added this directive to separate between the invite before the
+lesson and after lesson. This was the only way I could get the format
+of the invite form after the lesson not to change the way (good way)
+the invite before the lesson looked*/
+
+angular.module('lergoApp').directive('lessonCreateInviteFormAfter', function() {
+    return {
+        restrict: 'A',
+        templateUrl: 'views/lessons/intro/_inviteFormAfter.html',
+        scope: {
+            'onCloseAfter' : '&'
+        },
+        link: function($scope/*,element,attrs*/){
+
+            var Modes = {
+                CLASS: 'class',
+                STUDENT: 'student'
+            };
+
+            $scope.mode = Modes.CLASS;
+
+            $scope.setMode = function( mode ){
+                $scope.mode = mode;
+            };
+
+            $scope.setClassMode = function(){
+                $scope.setMode(Modes.CLASS);
+            };
+
+            $scope.setStudentMode = function(){
+                $scope.setMode(Modes.STUDENT);
+            };
+
+            $scope.isClassMode = function(){
+                return $scope.mode === Modes.CLASS;
+            };
+
+            $scope.isStudentMode = function(){
+                return $scope.mode === Modes.STUDENT;
+            };
+        }
+    };
+});
+
