ed0ea705d9fbf69e078cc9fe5b27c3d638ca89d2
jlevym <jlevym@gmail.com>
2018-03-27 09:21:40 +0300

    Congrats page - change scrollToTop to be in Write.js preventing scrollup when clicking on share or like

--- app/scripts/controllers/lessons/report/Write.js
+++ app/scripts/controllers/lessons/report/Write.js
@@ -43,6 +43,7 @@ var LessonsReportWriteCtrl = function ($scope, ReportWriteService, ReportsServic
     });
 
     $scope.$on('endLesson', function (/* event, data */) {
+        document.body.scrollTop = 0;  //  bring congrats page to top
         $log.info('lesson ended');
         // mark invitation as finished
         if (!report.data.finished) {
