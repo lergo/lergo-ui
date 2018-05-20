5649dc86ff6730e509d1a2cb2f0ac940ce38191a
jlevym <jlevym@gmail.com>
2018-05-20 16:39:32 +0300

    correct jshint failure

--- app/scripts/directives/selectAllTextOnClick.js
+++ app/scripts/directives/selectAllTextOnClick.js
@@ -18,7 +18,7 @@ angular.module('lergoApp')
                             if (document.execCommand('Copy')) {
                                 scope.copied = true;
                                 console.log('copied: ' + scope.copied);
-                                $timeout(function() {scope.copied = false; console.log('slept for a while')}, 2000);
+                                $timeout(function() {scope.copied = false; console.log('slept for a while');}, 2000);
 
                             }
                         } catch (e) {
