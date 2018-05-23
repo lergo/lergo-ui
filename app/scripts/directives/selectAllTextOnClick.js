fdec0f83cb78f3987e0bfeaa953acf56bbc27589
jlevym <jlevym@gmail.com>
2018-05-23 21:03:59 +0300

    copied to clipboard - make different message for pin and link

--- app/scripts/directives/selectAllTextOnClick.js
+++ app/scripts/directives/selectAllTextOnClick.js
@@ -10,12 +10,21 @@ angular.module('lergoApp')
     .directive('selectAllTextOnClick', function ($timeout) {
         return {
             restrict: 'A',
-            link: function (scope, element/*, attrs*/) {
+            link: function (scope, element, attrs) {
+                var getMessageText = function() {};
                 $(element).click(function () {
                     $timeout(function () {
                         element.select();
                         try {
                             if (document.execCommand('Copy')) {
+                                getMessageText =function(type) {
+                                    return {
+                                        pin: 'invitations.pinCopied',
+                                        link: 'invitations.linkCopied'
+                                    }[type];
+                                };
+                                var select = 'selectAllTextOnClick';
+                                scope.messageText = getMessageText(attrs[select]);
                                 scope.copied = true;
                                 console.log('copied: ' + scope.copied);
                                 $timeout(function() {scope.copied = false; console.log('slept for 5secs');}, 5000);
