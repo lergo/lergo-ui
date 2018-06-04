c09939419f146425ca9388b6a6f5831998f47e92
jlevym <jlevym@gmail.com>
2018-06-04 06:52:30 +0300

    fix( various pixel perfect) lesson summary space, lesson report number font-size, 3 bar dropdown placement, change order of menu in user dropdown, change order of tabs in user menu, change position of "not logged in" practice mistake popoup

--- app/scripts/controllers/BaseLayoutCreate.js
+++ app/scripts/controllers/BaseLayoutCreate.js
@@ -3,31 +3,31 @@
 angular.module('lergoApp').controller('BaseLayoutCreateCtrl', function($scope, $routeParams, $controller) {
 
 	$scope.sections = [ {
-		id : 'profile',
-		icon : 'fa fa-user',
-		controller : 'UsersProfileCtrl',
-		include : 'views/users/_userEditProfile.html'
+        id : 'invites',
+        icon : 'fa fa-envelope',
+        controller : 'InvitesIndexCtrl',
+        include : 'views/invites/_index.html'
 	}, {
-		id : 'lessons',
-		icon : 'fa fa-university',
-		controller : 'LessonsIndexCtrl',
-		include : 'views/lessons/_index.html'
-	}, {
-		id : 'questions',
-		icon : 'fa fa-question-circle',
-		controller : 'QuestionsIndexCtrl',
-		include : 'views/questions/_index.html'
-	}, {
-		id : 'reports',
-		icon : 'fa fa-bar-chart-o',
-		controller : 'ReportsIndexCtrl',
-		include : 'views/reports/_index.html'
-	}, {
-		id : 'invites',
-		icon : 'fa fa-envelope',
-		controller : 'InvitesIndexCtrl',
-		include : 'views/invites/_index.html'
+        id : 'reports',
+        icon : 'fa fa-bar-chart-o',
+        controller : 'ReportsIndexCtrl',
+        include : 'views/reports/_index.html'
 
+	}, {
+        id : 'lessons',
+        icon : 'fa fa-university',
+        controller : 'LessonsIndexCtrl',
+        include : 'views/lessons/_index.html'
+	}, {
+        id : 'questions',
+        icon : 'fa fa-question-circle',
+        controller : 'QuestionsIndexCtrl',
+        include : 'views/questions/_index.html'
+	}, {
+        id : 'profile',
+        icon : 'fa fa-user',
+        controller : 'UsersProfileCtrl',
+        include : 'views/users/_userEditProfile.html'
 	} ];
 
 	$scope.currentSection = _.find($scope.sections, function(section) {
