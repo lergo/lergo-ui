b15b543a4afd1f0c9a5a74ae4ac87d76f375e6d2
jlevym <jlevym@gmail.com>
2018-07-04 21:25:51 +0300

    starting to create play list module with createPlayList.js

---
+++ app/scripts/directives/createPlayList.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('lergoApp')
+    .directive('createPlayList', function (LergoClient, $location, $log) {
+        return {
+            templateUrl: 'views/directives/_createPlayList.html',
+            restrict: 'A',
+            link: function postLink($scope/*, element, attrs*/) {
+                $scope.create = function () {
+                    $scope.createBtnDisable=true;
+                    LergoClient.playlist.create().then(function (result) {
+                        var playlist = result.data;
+                        $scope.errorMessage = null;
+                        $location.path('/user/playlists/' + playlist._id + '/update');
+                    }, function (result) {
+                        $scope.errorMessage = 'Error in creating Playlist : ' + result.data.message;
+                        $log.error($scope.errorMessage);
+                        $scope.createBtnDisable=false;
+                    });
+                };
+            }
+        };
+    });
