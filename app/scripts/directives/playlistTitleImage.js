1068f8a5b06b61cc04e4fa380046f97dce16d03f
jlevym <jlevym@gmail.com>
2018-10-30 09:04:19 +0200

    playlist title image does not work

---
+++ app/scripts/directives/playlistTitleImage.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('lergoApp')
+    .directive('playlistTitleImage', function (LergoClient) {
+        return {
+            template: '<img class="img-responsive" ng-show="!!img" ng-src="{{img}}" width="100%" />' +
+            '<i ng-show="!img" class="fa fa-university"></i>',
+            restrict: 'A',
+            scope: {
+                'playlist': '='
+            },
+            link: function postLink(scope/*, element, attrs*/) {
+
+                scope.$watch('playList', function () {
+                    scope.img = LergoClient.playLists.getTitleImage(scope.playList);
+                });
+
+            }
+        };
+    });
