60e01f661c840b2bf8caea5da46138237d871124
jlevym <jlevym@gmail.com>
2018-11-12 21:16:13 +0200

    playlist preview works partially

---
+++ app/scripts/directives/lesson/lessonPreview.js
@@ -0,0 +1,28 @@
+/**
+ * Created by rahul on 5/9/16.
+ */
+'use strict';
+
+
+angular.module('lergoApp')
+    .directive('lessonPreview', function (LessonsService) {
+        return {
+            templateUrl: 'views/lessons/view/preview2/preview.html',
+            restrict: 'E',
+            transclude: true,
+            scope: {
+                'quizItem': '='
+            },
+            link: function ($scope) {
+
+                $scope.getLessonTemplate = function () {
+                   /* return 'views/lessons/view/preview2/preview.html';*/
+                    return 'views/lessons/view/preview2/_showLessons.html';
+                };
+
+                /*$scope.isFillInTheBlanks = function () {
+                    return lessonsService.lesson_TYPE.FILL_IN_THE_BLANKS === $scope.quizItem.type;
+                };*/
+            }
+        };
+    });
