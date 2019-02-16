44a9774874225b68fc1f521b89be0ba8794f66a8
jlevym <jlevym@gmail.com>
2019-02-16 11:33:20 +0200

    updating style of About.js

--- app/about/About.js
+++ app/about/About.js
@@ -1,62 +1,70 @@
-'use strict';
+(function(){
 
-angular.module('lergoApp').controller('AboutCtrl', function($scope, $routeParams, $window, $sce, $filter) {
-    $scope.sections = [
-        {
-            id: 'overview'
-        },
-        {
-            id: 'contribute'
+    'use strict';
+    AboutCtrl.$inject = ['$scope','$routeParams', '$window', '$sce','$filter'];
 
-        },
-        {
-            id: 'keyContributors'
+    function AboutCtrl($scope, $routeParams, $window, $sce, $filter) {
+        $scope.sections = [
+            {
+                id: 'overview'
+            },
+            {
+                id: 'contribute'
 
-        },
-        {
-            id: 'keyFunders'
+            },
+            {
+                id: 'keyContributors'
 
-        },
-        {
-            id: 'keyFundersPictures'
+            },
+            {
+                id: 'keyFunders'
 
-        },
-        {
-            id: 'lergoFeedback',
-            url: function () {
-                return $sce.trustAsResourceUrl($filter('translate')('about.sections.lergoFeedbackUrl'));
+            },
+            {
+                id: 'keyFundersPictures'
+
+            },
+            {
+                id: 'lergoFeedback',
+                url: function () {
+                    return $sce.trustAsResourceUrl($filter('translate')('about.sections.lergoFeedbackUrl'));
+                }
+            },
+            {
+                id: 'friends'
+
+            },
+            {
+                id: 'faq'
+
+            },
+            {
+                id: 'privacy'
+
+            },
+            {
+                id: 'contact'
+
             }
-        },
-        {
-            id: 'friends'
+        ];
+        $scope.scrollUp = function() {
+            $window.scrollTo(0, 0);
+        };
 
-        },
-        {
-            id: 'faq'
+        $scope.currentSection = _.find($scope.sections, function(section) {
+            return $routeParams.activeAboutTab === section.id;
+        });
 
-        },
-        {
-            id: 'privacy'
+        $scope.isActive = function(section) {
+            return !!$scope.currentSection && section.id === $scope.currentSection.id;
+        };
 
-        },
-        {
-            id: 'contact'
+        $scope.getInclude = function() {
+            return 'views/about/_' + $scope.currentSection.id + '.html';
+        };
+    }
 
-        }
-    ];
-	$scope.scrollUp = function() {
-		$window.scrollTo(0, 0);
-	};
+    angular.module('lergoApp').
+        controller('AboutCtrl', AboutCtrl );
 
-	$scope.currentSection = _.find($scope.sections, function(section) {
-		return $routeParams.activeAboutTab === section.id;
-	});
-
-	$scope.isActive = function(section) {
-		return !!$scope.currentSection && section.id === $scope.currentSection.id;
-	};
-
-	$scope.getInclude = function() {
-		return 'views/about/_' + $scope.currentSection.id + '.html';
-	};
-});
+}());
