125d21a3f1bc5c5189480c46db4a0a3f350d0812
4lergo <4lergo@gmail.com>
2020-06-16 17:51:31 +0300

    clean up for grunt

--- app/scripts/controllers/manage/users/ManageUsersIndex.js
+++ app/scripts/controllers/manage/users/ManageUsersIndex.js
@@ -44,9 +44,9 @@ angular.module('lergoApp')
             return _.map($scope.headers, $translate.instant);
         };
         $scope.getUsersFromSignUpDate = function(days) {
-            // if (days > 99) {
-            //     days = 99
-            // }
+            if (days > 99) {
+                days = 60;
+            }
             $log.debug('gettingUsersFromSignUpDate');
             LergoClient.users.getDate(days).then(function(result) {
                 
@@ -54,34 +54,14 @@ angular.module('lergoApp')
                 for (var i = 0; i < $scope.limitedUsers.length; i++) {
                     if (!$scope.limitedUsers[i].validated || $scope.limitedUsers[i].validated !== true) {
                         $scope.limitedUsers[i].validated = false;
-                        }
+                    }
                     $scope.limitedUsers[i].signupDate = $scope.getUserSignUpDate($scope.limitedUsers[i]);
                     delete $scope.limitedUsers[i]._id;  //remove the _id field
                 }
-                
-                console.log('..............', $scope.limitedUsers);
                 $scope.totalLimitedUsers = result.data.count;
             });
         };
-        // use this ng-csv users
-        $scope.getReports = function () {
-            var headers = $scope.localizedHeaders();
-                var data = _.map($scope.limitedUsers, function (item) {
-                    var report = {};
-                    report[headers[0]] = item.email;
-                    report[headers[1]] = item.fullName;
-                    report[headers[2]] = item.username;
-                    report[headers[3]] = item.validated;
-                    report[headers[4]] = item.signupDate
-                    console.log('the report is ', report);
-                    return report;
-                });
-                console.log('the data is: ', data);
-        };
     
-
-        
-
         $scope.getUserSignUpDate = function (user) {
             var signupDate = new Date(parseInt(user._id.substring(0, 8), 16) * 1000);
             return signupDate.toISOString().substring(0,10);
