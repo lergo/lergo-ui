de1a287bc5a8ae527e4aabfe9095327c313ee1e8
4lergo <4lergo@gmail.com>
2020-06-16 12:27:20 +0300

    csv download is working

--- app/scripts/controllers/manage/users/ManageUsersIndex.js
+++ app/scripts/controllers/manage/users/ManageUsersIndex.js
@@ -40,6 +40,10 @@ angular.module('lergoApp')
             });
         };
         $scope.days = '30';
+        $scope.localizedHeaders = function () {
+            return _.map($scope.headers, $translate.instant);
+        };
+        var userData = null;
         $scope.getUsersFromSignUpDate = function(days) {
             // if (days > 99) {
             //     days = 99
@@ -48,9 +52,39 @@ angular.module('lergoApp')
             LergoClient.users.getDate(days).then(function(result) {
                 $scope.limitedUsers = result.data.data;
                 console.log('..............', $scope.limitedUsers);
+                userData = $scope.limitedUsers;
                 $scope.totalLimitedUsers = result.data.count;
+                //ng-csv
+                // var headers = $scope.localizedHeaders();
+                
             });
         };
+        $scope.getUserData = function() {
+            return userData;
+        }
+
+        // $scope.getReports = function () {
+            
+        //     var headers = $scope.localizedHeaders();
+    
+        //         var data = _.map($scope.limitedUsers, function (item) {
+        //             console.log('item is ', item);
+        //             var report = {};
+        //             report[headers[0]] = item.data.lesson.name;
+        //             report[headers[1]] = item.data.invitee.name;
+        //             report[headers[2]] = item.data.invitee.class;
+        //             report[headers[3]] = $translate.instant('filters.subjects.'+item.data.lesson.subject);
+        //             report[headers[4]] = $filter('date')(item.lastUpdate, 'medium');
+        //             report[headers[5]] = item.correctPercentage;
+        //             if (item.duration !== 0) {
+        //                 report[headers[6]] = $filter('duration')(item.duration);
+        //             } else {
+        //                 report[headers[6]] = $translate.instant('report.incomplete');
+        //             }
+        //             return report;
+        //         }
+               
+        // };
 
        
 
