1a94c1636e409263ccd0eacdf29e906e35e053bc
Jeffrey <4lergo@gmail.com>
2020-10-29 13:33:04 +0200

    lergo-ui feat(validate signup on change password) for unvalidated user, initiating change password autovalidates the user

--- app/scripts/controllers/users/ChangePassword.js
+++ app/scripts/controllers/users/ChangePassword.js
@@ -2,6 +2,17 @@
 
 angular.module('lergoApp')
     .controller('UsersChangePasswordCtrl', function ($scope, $http, $routeParams, RequestProgressMonitor) {
+
+        // jeff: when user clicks on link to change password, the previous password will be validated
+        // this prevents the unpleasant situation that users forget the unvalidated password, and it 'loops'
+        $scope.validationInProgress = true;
+        $http.post('/backend/users/' + $routeParams._id + '/validate', { 'hmac': $routeParams.hmac}).then(function () {
+            $scope.validated = true;
+            $scope.validationInProgress = false;
+        }, function () {
+            $scope.validationInProgress = false;
+        });
+
         $scope.changePasswordForm = {  'hmac': $routeParams.hmac, 'userId': $routeParams._id  };
         $scope.changePasswordSuccess = false;
         $scope.changePasswordError = false;
