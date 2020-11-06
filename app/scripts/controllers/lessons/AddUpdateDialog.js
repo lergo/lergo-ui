6a3f0d9934e9d4dc48bd175c8546ed5bd77c6bb8
Jeffrey <4lergo@gmail.com>
2020-11-06 12:46:42 +0200

    removing "createNewLesson" from sections in AddUpdateDialog

--- app/scripts/controllers/lessons/AddUpdateDialog.js
+++ app/scripts/controllers/lessons/AddUpdateDialog.js
@@ -88,18 +88,7 @@ angular.module('lergoApp').controller('LessonsAddUpdateDialogCtrl',
 				}
 			}
 			$scope.sections = [ {
-				id : 'createNewLesson',
-				controller : 'LessonsUpdateCtrl',
-				tooltip : $filter('translate')('lessons.createNewLesson'),
-				page : 'views/lessons/_update.html',
-				isCreate : true,
-				add : function(item) {
-					addItem(item, false);
-				},
-				addClose : function(item) {
-					addItem(item, true);
-				}
-			}, {
+				
 				id : 'myLessons',
 				controller : 'LessonsPlaylistsIndexCtrl',
 				tooltip : $filter('translate')('lessons.selectMyLessons'),
@@ -149,7 +138,7 @@ angular.module('lergoApp').controller('LessonsAddUpdateDialogCtrl',
 			};
 
 			$scope.currentSection = _.find($scope.sections, function(section) {
-				return 'createNewLesson' === section.id;
+				return 'myLessons' === section.id;
 			});
 
             // function isCreateNewLessonSection (){
