b6e6327ce8ce9b7115b900d6ea6b74ab63f3eda5
Jeffrey <4lergo@gmail.com>
2024-01-24 08:00:33 +0200

    lergo-ui: change linkconverter for google to thumbnail for http and https

--- app/scripts/services/LergoResourceLinksConverter.js
+++ app/scripts/services/LergoResourceLinksConverter.js
@@ -20,15 +20,17 @@ angular.module('lergoApp')
          * @property {string} replace.to
          */
         this.LINK_TYPE = {
-            GOOGLE_DRIVE_1: { 'id' : 'google-drive', 'regex' : 'drive.google.com/file/d/', replace: { from: '(https://drive.google.com)/file/d/(.+)/.*', to: '$1/uc?export=view&id=$2'}},
-            GOOGLE_DRIVE_2: { 'id' : 'google-drive', 'regex' : 'drive.google.com/open\\?id=', replace:{ from: '(https://drive.google.com)/open\\?id=(.+)', to: '$1/uc?export=view&id=$2'}},
+            
+            GOOGLE_DRIVE_1: { 'id' : 'google-drive', 'regex' : 'drive.google.com/file/d/', replace: { from: 'https*://drive.google.com/file/d/(.+)/.*', to: 'https://drive.google.com/thumbnail?id=$1'}},
+            GOOGLE_DRIVE_2: { 'id' : 'google-drive', 'regex' : 'drive.google.com/open\\?id=', replace:{ from: 'https*://drive.google.com/open\\?id=(.+)', to: 'https://drive.google.com/uc?export=view&id=$1'}},
+            GOOGLE_DRIVE_3: { 'id' : 'google-drive', 'regex' : 'drive.google.com/uc\\?export=view&id=', replace: { from: 'https*://drive.google.com/uc\\?export=view&id=(.+)', to: 'https://drive.google.com/thumbnail?id=$1'}},
             YOUTUBE_1: { 'id' : 'youtube', 'regex' : 'youtu.be/', replace:{ from: '(https://youtu.be/.{11})\\?t=.+', to: '$1'}},
             YOUTUBE_2: { 'id' : 'youtube', 'regex' : 'youtube.com/watch\\?v=.{11}', replace:{ from: '(https://www.youtube.com/watch\\?v=.{11})&.+', to: '$1'}},
             YOUTUBE_3: { 'id' : 'youtube', 'regex' : 'youtube.com/watch\\?time_continue=.{2}&v=.{11}&', replace:{ from: '(https://www.youtube.com/watch\\?)time_continue=.{2}&(v=.{11})&.+', to: '$1$2'}},
             YOUTUBE_4: { 'id' : 'youtube', 'regex' : 'youtube.com/watch\\?time_continue=.{1}&v=.{11}&', replace:{ from: '(https://www.youtube.com/watch\\?)time_continue=.{1}&(v=.{11})&.+', to: '$1$2'}},
             LERGO_1: { 'id' : 'lergo', 'regex' : '.*\/user\/lessons\/.{24}\/intro', replace:{ from: '(.*)\/user\/(lessons\/.{24}\/intro)', to: '$1\/public\/$2'}}
         };
-
+                                                        
         this.getLinkType = function(link){
             return _.find(this.LINK_TYPE, function(t){
                 return  new RegExp(t.regex).test(link);
